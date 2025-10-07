f24e5e8c869a70f39af7d1bbc2ca74b406646812
LongPQ2 <longpq33@gmail.com>
2025-10-07 17:03:05 +0700

    feat: add footer

---
+++ template/page-footer.php
@@ -0,0 +1,43 @@
+<footer class="msb-page-footer">
+  <div class="top-container">
+      <div class="column">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/msb-logo-white.png" alt="icon-message" />
+          <p class="text-white">Ngân hàng TMCP Hàng Hải Việt Nam</p>
+          <span class="text-white">Số 54A Nguyễn Chí Thanh, Phường Láng Thượng, Quận Đống Đa, Thành phố Hà Nội, Việt Nam</span>
+      </div>
+      <div class="column">
+        <h3 class="text-white">Liên hệ</h3>
+        <p class="text-white">ATM và chi nhánh</p>
+        <div class="row">
+          <div class="icon-social">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/facebook.svg" alt="icon-facebook" />
+          </div>
+          <div class="icon-social">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/youtube.svg" alt="icon-youtube" />
+          </div>
+          <div class="icon-social">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/instagram.svg" alt="icon-instagram" />  
+          </div>
+          <div class="icon-social">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/zalo.png" alt="icon-zalo" />  
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <p class="text-white mb-0">Cá nhân 1900 6083</p>
+        <p class="text-white">Doanh nghiệp 1800 6260</p>
+        <p class="text-white">M-First 1800 599 999</p>
+      </div>
+      <div class="column">
+        <p class="text-white">MSB Digital Bank</p>
+        <div class="row">
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/google-store.png" alt="icon-message" />
+          <img src="<?php echo get_template_directory_uri(); ?>/assets/images/app-store.png" alt="icon-message" />
+        </div>
+       </div>
+    </div>
+
+  <div class="bottom-container">
+      <p>Bản quyền © 2026 thuộc về Ngân hàng TMCP Hàng Hải Việt Nam MSB</p>
+  </div>
+</footer>
